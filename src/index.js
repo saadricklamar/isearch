277a25099b8949719445414020e03aac10aa09cc
Saad Baradan <saadbaradan@gmail.com>
2019-09-08 17:35:28 -0600

    Install sass and add normalize file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './Scss/index.scss';
 import App from './Components/App/App';
 import * as serviceWorker from './serviceWorker';
 
