47a721d61485769e6b1ca93c71c08e831ab813fd
Saad Baradan <saadbaradan@gmail.com>
2019-09-10 17:10:56 -0600

    Install React router and add Home component with snpahsot

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import App from './App';
+import { Route, BrowserRouter} from 'react-router-dom';
 
 describe('App', () => {
   let wrapper = shallow(<App/>)
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(<BrowserRouter>
+                      <Route path="/" component={App} />
+                    </BrowserRouter>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
   it('should match the snapshot', () => {
