14e1c46cc248f3765ad0a7790639a47ccab8c060
Saad Baradan <saadbaradan@gmail.com>
2019-09-15 16:29:24 -0600

    Add search component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Search from '../Search/Search'
 
-const Home = () => {
-    return(
-    <header>
-        <h1>iSearch</h1>
-        <nav>
-            <Link className="aboutLink" to="/About">
-            About
-            </Link>
-        </nav>
-    </header>
-    )
+export class Home extends Component  {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+    
+
+
+
+
+    render () {
+        return(
+            <div>
+            <header>
+                <h1 className="h1">iSearch</h1>
+                <nav>
+                    <Link className="about-link" to="/About">
+                    About
+                    </Link>
+                </nav>
+            </header>
+            <Search />
+            </div>
+
+            )
+
+
+    }
+   
 }
 
 export default Home;
