3d5ff0c712496b7b2c9f4454ed92ee3220de361e
Saad Baradan <saadbaradan@gmail.com>
2019-09-16 17:16:03 -0600

    Make utilities folder where helper functions will go

---
+++ src/Utilities/cleanUserInput.js
@@ -0,0 +1,5 @@
+export const cleanUserInput = () => {
+    //this function will clean the user input of 
+    //of spaces, caps, etc and ensure it is in the
+    //proper format when making the API call
+};
