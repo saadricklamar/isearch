de62d4bad2f63b50dd9383e7f39a7fe0034f3d7c
Saad Baradan <saadbaradan@gmail.com>
2019-10-15 17:11:59 -0600

    Use listgroup to display television results

--- src/Components/Television/Television.js
+++ src/Components/Television/Television.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { ListGroup } from "react-bootstrap";
 
-const Television = () => {
+const Television = props => {
   return (
     <section>
       <h3>Television</h3>
-      <p>This section will contain Television related to your search</p>
+      <ListGroup className="list">
+        {props.results.map(item => {
+          if (item.kind === "tv-episode") {
+            return (
+              <ListGroup.Item variant="info" className="track-name">
+                {item.trackName}
+              </ListGroup.Item>
+            );
+          }
+        })}
+      </ListGroup>
     </section>
   );
 };
