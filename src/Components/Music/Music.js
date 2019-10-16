8678683221e13cefc36fa183cb0f9b6a54d47f36
Saad Baradan <saadbaradan@gmail.com>
2019-10-16 11:14:39 -0600

    Write function that converts track length in millis to minutes/seconds

--- src/Components/Music/Music.js
+++ src/Components/Music/Music.js
@@ -2,6 +2,11 @@ import React from "react";
 import { ListGroup } from "react-bootstrap";
 
 const Music = props => {
+  function convertToMinutes(millis) {
+    let minutes = Math.floor(millis / 60000);
+    let seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  }
   return (
     <section>
       <h3>Music</h3>
@@ -16,6 +21,9 @@ const Music = props => {
                   <p className="item">Artist: {item.artistName} </p>
                   <p className="item">Album Price: ${item.collectionPrice}</p>
                   <p className="item">Track Price: ${item.trackPrice}</p>
+                  <p className="item">
+                    Length: {convertToMinutes(item.trackTimeMillis)}
+                  </p>
                 </ListGroup.Item>
               </a>
             );
