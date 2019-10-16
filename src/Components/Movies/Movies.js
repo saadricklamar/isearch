2e2e97845e7db515f425a132045b5eaed5f6bb6f
Saad Baradan <saadbaradan@gmail.com>
2019-10-16 13:01:19 -0600

    Link movie cards to itunes and display movie poster

--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -9,9 +9,15 @@ const Movies = props => {
         {props.results.map(item => {
           if (item.kind === "feature-movie") {
             return (
-              <ListGroup.Item variant="info" className="track-name">
-                {item.trackName}
-              </ListGroup.Item>
+              <a href={item.trackViewUrl} target="_blank">
+                <ListGroup.Item variant="warning" className="movie">
+                  <img src={item.artworkUrl100} alt="Movie Poster"></img>
+                  <p className="item">Movie: {item.trackName} </p>
+                  <p className="item">Producer: {item.artistName} </p>
+                  <p className="item">Buy: ${item.trackPrice}</p>
+                  <p className="item">Rent ${item.trackRentalPrice}</p>
+                </ListGroup.Item>
+              </a>
             );
           }
         })}
